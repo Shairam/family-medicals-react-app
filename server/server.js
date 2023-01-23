2e833973dbdb58175fb336aaa5bf8f68b158490d
shairam <shaikrish27@gmail.com>
2023-01-23 12:33:46 +0000

    Deploy to Heroku

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-const express = require('express');
+const express = require('express'); 
 const mongoose = require('mongoose');
+const path = require("path")
 
 const mongoString = "mongodb+srv://shairam:Sriganthi_2426@learnmongo.wqv4poy.mongodb.net/Medical_data?retryWrites=true&w=majority"
 
@@ -7,38 +8,45 @@ mongoose.connect(mongoString);
 const database = mongoose.connection
 console.log(mongoString)
 database.on('error', (error) => {
-    console.log(error)
+  console.log(error)
 })
 
 database.once('connected', () => {
-    console.log('Database Connected');
+  console.log('Database Connected');
 })
 
 const routes = require('./routes/routes');
-
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-    //intercepts OPTIONS method
-    if ('OPTIONS' === req.method) {
-      //respond with 200
-      res.send(200);
-    }
-    else {
+  //intercepts OPTIONS method
+  if ('OPTIONS' === req.method) {
+    //respond with 200
+    res.send(200);
+  }
+  else {
     //move on
-      next();
-    }
+    next();
+  }
 });
-  
+
 
 app.use('/api', routes)
 
-app.listen(4000, () => {
-    console.log(`Server Started at ${4000}`)
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../build', 'index.html')); // relative path
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server Started at ${PORT}`)
 })
